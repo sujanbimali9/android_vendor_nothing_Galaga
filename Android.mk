291783f70a2747ad9fd51a5b8a4a1d876b24b8c1
sm455 <221133194+sm455@users.noreply.github.com>
2026-02-27 07:45:26 +0000

    Galaga: Initial import from Galaga_B4.0-260108-1654

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),Galaga)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,c0cd7563419d0a36cf220eae69f85e8eaa4c2aa5)
+$(call add-radio-file-sha1-checked,radio/ccu.img,87f8c001abc4272003e547155f342cafb84b8feb)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,a4ba2362b934a7bd6a07149c5a28ee72d093b6db)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,6464f86e27e6d219b716c2ff27fdccbe93bcff8e)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,2e454454372e540bba86d390ecc67276744507ff)
+$(call add-radio-file-sha1-checked,radio/dpm.img,b4b58a79670cb18c3f5516aa69524da999fcd368)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,b5841226274fadf679547118496882613cb48a8e)
+$(call add-radio-file-sha1-checked,radio/gz.img,f5ffa6cf84c9a13c447daa905d83eab6088adb66)
+$(call add-radio-file-sha1-checked,radio/lk.img,e27f9c1b2019d63627fa32afb0276b5b8aa6528b)
+$(call add-radio-file-sha1-checked,radio/logo.img,c0f59debcb0c1a02484fe69b4451321a619adc35)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,4377cf88476cb05eda23dd906029eb57017fd38a)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,f0bca3418a9e267fc1f989f592a357cf9a45f548)
+$(call add-radio-file-sha1-checked,radio/modem.img,d18a7f2fc7353cc1445cc7a30541936ce3742361)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,387e2d5309db6a76ff247ad35788e57b8cd0e38c)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,ada4942b6e5f45b6b335613fd6f5da92efbe179a)
+$(call add-radio-file-sha1-checked,radio/scp.img,53cabccd361e24e6811f593bec850e7e20e2eebc)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,3a7d4082047114b331a570c280e1cc4d7850d909)
+$(call add-radio-file-sha1-checked,radio/sspm.img,810c43e4edda79823a207b2be377c6d870fd4b48)
+$(call add-radio-file-sha1-checked,radio/tee.img,dd4a6e0c8121d08dad5c10e1c0f16537c89b3e6e)
+$(call add-radio-file-sha1-checked,radio/vcp.img,f09828abd2a7edac9f63cf9e1dabb46438895162)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,25 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    apusys \
+    ccu \
+    connsys_bt \
+    connsys_gnss \
+    connsys_wifi \
+    dpm \
+    gpueb \
+    gz \
+    lk \
+    logo \
+    mcf_ota \
+    mcupm \
+    modem \
+    pi_img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    vcp
